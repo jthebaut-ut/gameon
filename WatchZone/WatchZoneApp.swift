19c8066d1862c130b72fdf7cd5f509bedd067005
jthebaut <jthebaut@gmail.com>
2026-05-02 11:27:08 -0600

    Initial Commit

---
+++ WatchZone/WatchZoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchZoneApp.swift
+//  WatchZone
+//
+//  Created by Jonathan Thebaut on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WatchZoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
